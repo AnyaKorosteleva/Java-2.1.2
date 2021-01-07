ddc1752209c694c7530a23fbf0def3ce8ff1c978
AnyaKorosteleva <74379643+AnyaKorosteleva@users.noreply.github.com>
2021-01-07 21:52:50 +0300

    formaring code

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-
         int refill = 1000;
         int bonus = 1000;
         int accaunt = 200;
         int balance;
-        int commonBonus = refill / 100;
-        if (refill < bonus) {commonBonus = 0;}
-        if (refill < bonus) {balance = refill + accaunt ;}
-        else {balance = refill + accaunt + commonBonus ;}
+        int commonbonus = refill / 100;
+        if (refill < bonus) {
+            commonbonus = 0;
+        }
+        if (refill < bonus) {
+            balance = refill + accaunt;
+        } else {
+            balance = refill + accaunt + commonbonus;
+        }
         System.out.println(balance);
-        System.out.println(commonBonus);
+        System.out.println(commonbonus);
 
     }
 }
