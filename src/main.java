431c9b5c21bdf59fd95d24e019817d6222305e5d
AnyaKorosteleva <74379643+AnyaKorosteleva@users.noreply.github.com>
2021-01-07 20:29:33 +0300

    Correct Balance and Bonus

--- src/main.java
+++ src/main.java
@@ -1,13 +1,14 @@
 public class main {
     public static void main(String[] args) {
 
-        int refill = 1100;
-        int EveryHundred = 100;
+        int refill = 1000;
         int bonus = 1000;
-        int accaunt = 500;
-        if (refill < bonus) {refill = 0;}
-        int CommonBonus = refill/EveryHundred;
-        int Balance = refill + accaunt + CommonBonus;
+        int accaunt = 200;
+        int Balance;
+        int CommonBonus = refill/100;
+        if (refill < bonus) {CommonBonus = 0;}
+        if (refill < bonus) {Balance = refill + accaunt ;}
+        else {Balance = refill + accaunt + CommonBonus ;}
         System.out.println(Balance);
         System.out.println(CommonBonus);
 
