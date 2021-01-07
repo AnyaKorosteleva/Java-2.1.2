3740cda96eff0af31b5449be8d0c7de209cb45bb
AnyaKorosteleva <74379643+AnyaKorosteleva@users.noreply.github.com>
2021-01-07 20:45:08 +0300

    formatting rules

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int refill = 1000;
+        int bonus = 1000;
+        int accaunt = 200;
+        int balance;
+        int commonBonus = refill/100;
+        if (refill < bonus) {commonBonus = 0;}
+        if (refill < bonus) {balance = refill + accaunt ;}
+        else {balance = refill + accaunt + commonBonus ;}
+        System.out.println(balance);
+        System.out.println(commonBonus);
+
+    }
+}
